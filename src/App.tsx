6021fb43ae15662b9089cd874932eaf710884474
Nazim-Yoldas Sesen <nazimsesen@gmail.com>
2023-05-03 12:43:06 -0400

    Added Footer

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import { Header } from "./components/Header";
 import { Skills } from "./components/Skills";
 import { Projects } from "./components/Projects";
 import { Contact } from "./components/Contact";
-// import { Footer } from "./components/Footer";
+import { Footer } from "./components/Footer";
 
 function App() {
   return (
@@ -20,6 +20,7 @@ function App() {
           <Skills />
           <Projects />
           <Contact />
+          <Footer />
         </div>
       </>
     </Router>
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,1 +1,9 @@
-// export const Footer = () => {};
+export const Footer = () => {
+  return (
+    <div className="footer-section">
+      <div className="footer-title">
+        <span>2023</span>
+      </div>
+    </div>
+  );
+};
