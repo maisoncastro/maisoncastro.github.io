7f0b1a81be64de6dffbf3116e0d04de2959ee183
Nazim-Yoldas Sesen <nazimsesen@gmail.com>
2024-05-28 16:03:29 -0400

    Added Lenis

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,40 @@
-// import { useEffect, useRef } from "react";
-// import LocomotiveScroll from "locomotive-scroll";
-
 import { BrowserRouter as Router } from "react-router-dom";
 import "./App.css";
-
-// Components
-
+import Lenis from "lenis";
 import { NavBar } from "./components/NavBar";
 import { Header } from "./components/Header";
 import { Skills } from "./components/Skills";
 import { Projects } from "./components/Projects";
 import { Contact } from "./components/Contact";
 import { Footer } from "./components/Footer";
+import { useEffect, useRef } from "react";
 
 function App() {
-  // const scrollContainerRef = useRef(null);
+  useEffect(() => {
+    const lenis = new Lenis({
+      lerp: 0,
+    });
 
-  // useEffect(() => {
-  //   if (scrollContainerRef.current) {
-  //     const scroll = new LocomotiveScroll({
-  //       el: scrollContainerRef.current,
-  //       smooth: true,
-  //     });
+    function raf(time: number) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
 
-  //     return () => {
-  //       if (scroll) {
-  //         scroll.destroy();
-  //       }
-  //     };
-  //   }
-  // }, []);
+    requestAnimationFrame(raf);
+  }, []);
 
   return (
     <Router>
-      <>
-        <div
-          className="app-container"
-          // data-scroll-container
-          // ref={scrollContainerRef}
-        >
-          <div className="content-container">
-            <NavBar />
-            <Header />
-            <Skills />
-            <Projects />
-            <Contact />
-            <Footer />
-          </div>
+      <div className="app-container">
+        <div className="content-container">
+          <NavBar />
+          <Header />
+          <Skills />
+          <Projects />
+          <Contact />
+          <Footer />
         </div>
-      </>
+      </div>
     </Router>
   );
 }
